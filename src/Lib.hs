eca2ce36e4e118fd5ada88aff8c1acdd84c9dbff
Pieter Brandwijk <pieter@pbrandwijk.com>
2017-12-31 12:59:26 +0100

    Add checking functions for pre and postconditions

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,4 @@
-module Lib
-    ( assignSanta
-    ) where
+module Lib where
 
 import Shuffle (shuffle)
 import System.IO.Unsafe (unsafePerformIO)
@@ -20,23 +18,37 @@ everyNameUnique = allUnique
 
 -- PROGRAM
 
+checkPreconditions :: [String] -> Bool
+checkPreconditions xs =
+  if minimalThree xs == False then error "Group must have at least three members!"
+  else if everyNameUnique xs == False then error "All member names must be unique!"
+  else True
+
 assignSanta :: [String] -> [(String, String)]
 assignSanta s = if nonReflexive assn then assn else assignSanta s
   where
     assn = zip s (unsafePerformIO $ shuffle s)
 
--- Relation is non-reflexive; no one is santa of himself.
-nonReflexive :: (Eq a) => [(a,a)] -> Bool
-nonReflexive [] = True
-nonReflexive ((s,r):xs) | s /= r = True && nonReflexive xs
-                        | otherwise = False
+checkPostconditions :: [(String,String)] -> Bool
+checkPostconditions xs =
+  if nonReflexive xs == False then error "No member can be santa to himself!"
+  else True
 
 
 -- POSTCONDITIONS
 
 -- All members occur as santa once (if (x,y), then no (x,z))
+
 -- All members occur as receiver once (if (x,y), then no (z,y))
+
 -- There are no santas that are not members (if (x,y), then also (z,x))
+
 -- There are no receivers that are not members (if (x,y), then also (y,z))
--- No member is santa to himself (no (x,x))
+
+-- Relation is non-reflexive; no one is santa of himself (no (x,x))
+nonReflexive :: (Eq a) => [(a,a)] -> Bool
+nonReflexive [] = True
+nonReflexive ((s,r):xs) | s /= r = True && nonReflexive xs
+                        | otherwise = False
+
 -- ?No two members are santa to each other (if (x,y), then no (y,x))
